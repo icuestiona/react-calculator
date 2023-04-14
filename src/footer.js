8fe41c64d4d24c54251fae2399e5cca8d0541379
Soraya <icuestiona@gmail.com>
2023-04-15 00:33:16 +0200

    Footer component added

---
+++ src/footer.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      Coded by{" "}
+      <a
+        href="https://www.linkedin.com/in/sorayacarvajal/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Soraya Carvajal
+      </a>{" "}
+      and is open-sourced on {""}
+      <a
+        href="https://github.com/icuestiona/react-calculator"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github
+      </a>
+    </footer>
+  );
+}
